78537a1657a0e3947de72e3a3869aae0ac51ce3a
Elisa Müller <elisa.mullerdiaz@gmail.com>
2026-03-05 12:36:56 +0100

    Initial commit

---
+++ data_extraction.py
@@ -0,0 +1,4 @@
+'''
+In this part of the assignment you will have to create a graph following the model designed in the previous section.
+You must (partially) instantiate your graph using real data, e.g., load data from Semantic Scholar, DBLP, or any other data source that you may find.
+'''
